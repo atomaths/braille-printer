135f19f8e365f2a3518064b18a2298314c3f4ecb
Homin Lee <suapapa@insignal.co.kr>
2012-09-17 21:00:38 +0900

    Fixed example to hangul braille

--- src/main.go
+++ src/main.go
@@ -7,7 +7,8 @@ package helloworld
 import (
 	"fmt"
 	"net/http"
-	"github.com/suapapa/go_braille"
+	//brl "github.com/suapapa/go_braille"
+	brl_ko "github.com/suapapa/go_braille/ko"
 )
 
 func init() {
@@ -15,7 +16,7 @@ func init() {
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	s := "Hello World"
+	s := "오빤 강남 스타일"
 	fmt.Fprint(w, s + "\n")
-	fmt.Fprint(w, braille.Encode(s) + "\n")
+	fmt.Fprint(w, brl_ko.Encode(s) + "\n")
 }
