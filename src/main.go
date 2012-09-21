17959a0147d72445d7e19e759cb8b6ffab6a49e7
Jongmin Kim <atomaths@gmail.com>
2012-09-21 21:30:06 +0900

    Add /braille API and http://braille-printer.appspot.com index page

--- src/main.go
+++ src/main.go
@@ -1,21 +1,46 @@
-// Copyright 2011 Google Inc. All rights reserved.
-// Use of this source code is governed by the Apache 2.0
-// license that can be found in the LICENSE file.
+// Copyright 2012 Braille Printer Team. All rights reserved.
+// Use of this source code is governed by the Apache 2.0 license.
 
-package helloworld
+package brailleprinter
 
 import (
 	"fmt"
 	"net/http"
-	"github.com/suapapa/go_braille"
+	braille "github.com/suapapa/go_braille"
 )
 
 func init() {
-	http.HandleFunc("/", handle)
+	http.HandleFunc("/braille", brailleHandler)
+	http.HandleFunc("/root", rootHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/", indexHandler)
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	s := "Hello World"
-	fmt.Fprint(w, s + "\n")
+// http://braille-printer.appspot.com/ 첫 페이지 출력
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+	/*
+	s := "동해물과"
+	fmt.Fprint(w, "<!DOCTYPE html><html><head></head><body>")
+	fmt.Fprint(w, s + "<br>")
 	fmt.Fprint(w, braille.Encode(s) + "\n")
+	fmt.Fprint(w, "</body></html>")
+	*/
+}
+
+// root 로그인 화면 출력
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "root.html")
+}
+
+// root 로그인 처리
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	rootInsert(w, r)
+}
+
+func brailleHandler(w http.ResponseWriter, r *http.Request) {
+	src := r.FormValue("b-input")
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, braille.Encode(src))
 }
---
+++ static/index.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+    $("#result").focus(function() {
+        $(this).blur();
+    });
+    $("#result").autoGrow();
+    $("#b-input").focus();
+    $("#b-input").keyup(function(e) {
+        if (e.keyCode == 13) {
+            do_braille();
+        }
+    });
+    $("#btn-braille").click(do_braille);
+    $("#btn-print").click(do_print);
+});
+
+// 입력한 내용을 서버에 /braille API 호출로 점자로 변환.
+function do_braille() {
+    var inputval = $.trim($("#b-input").val());
+    if (inputval == "") {
+        $("#b-input").focus();
+        return false;
+    }
+
+    $.post("/braille", "b-input=" + inputval, function(result) {
+        $("#ly-result").show();
+        $("#result").html(result);
+        $("#result").autoGrow();
+        $("#b-input").blur();
+    });
+}
+
+function do_print() {
+    alert($("#result").html());
+}
+
+
+// textarea auto grow plugin
+jQuery.fn.autoGrow = function(){
+    return this.each(function(){
+        // Variables
+        var colsDefault = this.cols;
+        var rowsDefault = this.rows;
+
+        //Functions
+        var grow = function() {
+            growByRef(this);
+        }
+
+        var growByRef = function(obj) {
+            var linesCount = 0;
+            var lines = obj.value.split('\n');
+
+            for (var i=lines.length-1; i>=0; --i)
+    {
+        linesCount += Math.floor((lines[i].length / colsDefault) + 1);
+    }
+
+    if (linesCount >= rowsDefault)
+        obj.rows = linesCount + 1;
+    else
+        obj.rows = rowsDefault;
+        }
+
+        var characterWidth = function (obj){
+            var characterWidth = 0;
+            var temp1 = 0;
+            var temp2 = 0;
+            var tempCols = obj.cols;
+
+            obj.cols = 1;
+            temp1 = obj.offsetWidth;
+            obj.cols = 2;
+            temp2 = obj.offsetWidth;
+            characterWidth = temp2 - temp1;
+            obj.cols = tempCols;
+
+            return characterWidth;
+        }
+
+        // Manipulations
+        this.style.height = "auto";
+        this.style.overflow = "hidden";
+        //this.style.width = ((characterWidth(this) * this.cols) + 6) + "px";
+        //this.onkeyup = grow;
+        //this.onfocus = grow;
+        //this.onblur = grow;
+        growByRef(this);
+    });
+};
+
